dca8a092a4633bce36eec5e139590c807e7055c9
cattykitty4 <aspectmastezzima@gmail.com>
2025-01-13 19:10:51 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from previous_occurrence import find_previous_occurrence
-from less_in_alphabetical_order import find_less_in_alph_order
-from counting_occurrences import counting_index
+from .previous_occurrence import find_previous_occurrence
+from .less_in_alphabetical_order import find_less_in_alph_order
+from .counting_occurrences import counting_index
 
 __all__ = ('find_previous_occurrence', 'find_less_in_alph_order', 'counting_index')
